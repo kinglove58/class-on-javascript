fbe492d6d8043a861eb17e145ca7bd0f7c80c775
Techking <Kprincejah@gmail.com>
2024-01-16 14:15:22 +0100

    challenge

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-dice1.png
+function rollDice(PlayerIndex) {
+  let randomNumber = Math.floor(Math.random() * 6) + 1;
+  let randomImage = "img/dice" + randomNumber + ".png";
+  document
+    .querySelectorAll("img")
+    [PlayerIndex].setAttribute("src", randomImage);
+  return randomNumber;
+}
+
+let randomNumber1 = rollDice(0);
+let randomNumber2 = rollDice(1);
+
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "Player 1 wins😂";
+} else if (randomNumber2 > randomNumber1) {
+  document.querySelector("h1").innerHTML = "Player 2 wins😅";
+} else {
+  document.querySelector("h1").innerHTML = "Draw😒";
+}
