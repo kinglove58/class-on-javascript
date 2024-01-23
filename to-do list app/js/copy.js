3f6915a61c98253d337ab2797f30e436714c62ba
Techking <Kprincejah@gmail.com>
2024-01-23 14:54:20 +0100

    practice

--- to-do list app/js/copy.js
+++ to-do list app/js/copy.js
@@ -17,4 +17,75 @@ const initApp = () => {
     event.preventDefault();
     processSubmission();
   });
+
+  const clearItem = document.querySelector("#clearItems");
+  clearItem.addEventListener("click", (event) => {
+    const list = toDoList.getList();
+    if (list.length) {
+      const confirmed = confirm("Are you sure you want to delete your items");
+
+      if (confirmed) {
+        toDoList.clearList();
+        updatePersistentData(toDoList.getList());
+        refreshThePage();
+      }
+    }
+  });
+  //procedure
+  loadListObject();
+  refreshThePage();
+};
+
+const loadListObject = () => {
+  const storedList = localStorage.getItem("myToDoList");
+  if (typeof storedList !== "string") return;
+  const parsedList = JSON.parsedList.forEach((itemObj) => {
+    const newToDoItem = createNewItem(itemObj._id, itemObj.item);
+    toDoList.addItemToDoList(newToDoItem);
+  });
+};
+
+const refreshThePage = () => {
+  clearListDisplay();
+  renderList();
+  clearItemEntryField();
+  setFocusOnItemEntry();
+};
+
+const clearListDisplay = () => {
+  const parentElement = document.querySelector("#listItem");
+  deleteContents(parentElement);
+};
+
+const renderList = () => {
+  const list = toDoList.getList();
+  list.forEach((item) => {
+    buildListItem(item);
+  });
+};
+
+const buildListItem = () => {
+  const div = document.createElement("div");
+  div.className = "item";
+  const check = document.createElement("check");
+  check.type = "checkbox";
+  check.id = item.getId();
+  check.tabIndex = 0;
+  addClickListenerTocheckbox(check);
+  const label = document.createElement("label");
+  label.htmlFor = item.getId();
+  label.textContent = item.getItem();
+  div.appendChild(check);
+  div.appendChild(label);
+  const container = document.querySelectorAll("#listItems");
+  container.appendChild(div);
+};
+
+const addClickListenerTocheckbox = (checkbox) => {
+  checkbox.addEventListener("click", (event) => {
+    if (checkbox.checked) {
+      toDoList.removeItemFromList(checkbox.id);
+      updatePersistentData(toDoList.getList());
+    }
+  });
 };
